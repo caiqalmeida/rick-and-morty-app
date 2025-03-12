5a175c00ae0987603b954435a4ae0c9953a27ee7
Caique Almeida <caaguima@gmail.com>
2025-03-12 09:39:16 -0300

    feat: search bar logic

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,11 +1,36 @@
-import classes from './index.module.scss'
+import classes from './index.module.scss';
 
-export function SearchBar () {
+interface SearchBarProps {
+  isSearching: boolean;
+  searchName: string;
+  setSearchName: (search: string) => void;
+  handleSearch: () => void;
+  handleClearSearch: () => void;
+}
+
+export function SearchBar ({isSearching, searchName, setSearchName, handleSearch, handleClearSearch} : SearchBarProps) {
 
   return (
-    <div className={classes['search-bar__wrapper']}>
-      <input className={classes['search-bar__input']} type="text" placeholder="Pesquisar personagem"/>
-      <button className={classes['search-bar__button']}>Pesquisar</button>
+    <>
+      <div className={classes['search-bar__wrapper']}>
+      <input 
+      className={classes['search-bar__input']} 
+      type="text" 
+      placeholder="Search for character name"  
+      onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchName(event.target.value);
+      }}
+      value={searchName}
+      />
+      <button onClick={handleSearch} className={classes['search-bar__button']}>Search</button>
     </div>
+    {isSearching && (  
+    <div className={classes['search-bar__info']}>
+      <span>Searching for: {searchName} </span>
+      <button onClick={handleClearSearch} className={classes['search-bar__button']}>X Cancel search</button>
+    </div> 
+    )}
+
+    </> 
   )
 }
